d0ada2d64350c7ecf90b68d5bc75d4b2dd4463c5
CarterMcAlister <5679378+CarterMcAlister@users.noreply.github.com>
2026-06-08 23:18:37 -0400

    Allow dynamic ngrok TCP endpoint

--- docker/ngrok/start-ngrok-tcp.sh
+++ docker/ngrok/start-ngrok-tcp.sh
@@ -2,9 +2,12 @@
 set -eu
 
 : "${NGROK_AUTHTOKEN:?NGROK_AUTHTOKEN is required}"
-: "${NGROK_TCP_URL:?NGROK_TCP_URL is required}"
 
 NGROK_UPSTREAM="${NGROK_UPSTREAM:-alliance-postgres-18:10000}"
 export NGROK_AUTHTOKEN
 
-exec ngrok tcp "$NGROK_UPSTREAM" --url "$NGROK_TCP_URL"
+if [ -n "${NGROK_TCP_URL:-}" ]; then
+  exec ngrok tcp "$NGROK_UPSTREAM" --url "$NGROK_TCP_URL"
+fi
+
+exec ngrok tcp "$NGROK_UPSTREAM"
